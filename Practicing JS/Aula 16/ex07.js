570567a5b31fcdd16c347dd1755b858f24ab99f2
Belmiro00s <92956017+belmiro00s@users.noreply.github.com>
2023-10-10 16:51:19 -0300

    Changes: Fix function finishi

--- Practicing JS/Aula 16/ex07.js
+++ Practicing JS/Aula 16/ex07.js
@@ -1,120 +1,92 @@
 var tabu = document.querySelector("select#tab");
 var numero = document.querySelector("input#numeros");
 var res = document.querySelector("div#result");
-let analy =  []
-
-
-
+let analy = []
 
 
 function addnumber() {
-  if ((numero.value > 100) | (numero.value == 0)) {
+  if ((numero.value > 100) || (numero.value == 0)) {
     window.alert("insira um numero válido");
   } else {
-    let item = document.createElement("option");
-    item.text = numero.value;
-    tabu.appendChild(item);
-    item.innerHTML =  `  ${numero.value} adicionado `
-    analy.push(numero.value)
-     
+    let item = document.createElement("option"); //criando o elemento option
+    item.text = numero.value;                    //elemento item recebe numero inserido
+    tabu.appendChild(item);                      // adicionando o elemento item ao elemento select
+    item.innerHTML = `  ${numero.value} adicionado ` // adicionando o numero inserido dentro de item 
+    analy.push(numero.value) // inserindo o numero que foi dado dentro da variavel composta analy
+  
+    
     console.log(analy)
     
+    numero.value = ""
   }
-    
- 
 
-}
-
-function sumnumber(){
-  
-  var totalnumbers = analy.length
-
-
-if(analy.length > 1 ){
-  res.innerHTML =  `Ao todo temos ${totalnumbers} numeros cadastrados`
-
-}else {
-  res.innerHTML =  `Ao todo temos ${totalnumbers} numero cadastrado`
-}
-}
   
 
+}
 
 
-function valuenumber(){
+function finish() {
 
-  let largest = analy.reduce(function (a,b){ // reduzindo todos os valores em um unico valor, ou nesses 2 parametros
-      return (a > b ) ? a : b     //aqui compara quem tem o maior valor 
+  var countnumberlist = analy.length
+  let sum = 0
+
+  if(analy.length == 0 ){
+    window.alert("Adicione primeiro um numero na lista")
+  }else{
+
+
+  
+
+  let largest = analy.reduce(function (a, b) { // reduzindo todos os valores em um unico valor, ou nesses 2 parametros
+    return (a > b) ? a : b     //aqui compara quem tem o maior valor 
   })
 
-  let smaller = analy.reduce(function (a,b){ // reduzindo todos os valores em um unico valor, ou nesses 2 parametros
-    return (a < b ) ? a : b     //aqui compara quem tem o maior valor 
-})
+  let smaller = analy.reduce(function (a, b) { // reduzindo todos os valores em um unico valor, ou nesses 2 parametros
+    return (a < b) ? a : b     //aqui compara quem tem o menor valor 
+  })
 
-res.innerHTML = ` O menor valor informado foi: ${smaller} ` 
+  for (let i = 0; i < analy.length; i++) {
 
- res.innerHTML = ` O maior valor informado foi: ${largest} ` 
-   
-      
+    let number = parseFloat(analy[i])
 
-  }
-
-
-function valuenumber2(){
-
-let smaller = analy.reduce(function (a,b){
-return (a <  b) ? a : b
-
-})
-res.innerHTML = ` O menor valor informado foi: ${smaller} `
-}
-
-
-
-
-
-
-function sumall(){
-  let sum = 0 
-
-  for (let i = 0; i < analy.length; i++){
-    
-    let number = parseFloat (analy[i])
-  
     sum += number
-
-  
-
-   
-  
-
-  res.innerHTML = `A soma de todos os valores é: ${sum}`
-  console.log(sum)
-  
-  
   }
-}
-
- function media(){
-
-  let sum = 0 
-
-  for (let i = 0; i < analy.length; i++){
-    
-  let number = parseFloat (analy[i])
-  
-  sum += number
 
   var medias = (sum / analy.length)
 
-    
-   }
-  console.log(medias)
-  res.innerHTML = `A media de todos os valores é: ${medias}`
-  console.log(mediatotal)
+
+
+  var resultText = `A soma de todos os valores é: ${sum}<br>`;
+  resultText += `O total de números informados é: ${countnumberlist}<br>`;
+  resultText += `O menor valor informado foi: ${smaller}<br>`;
+  resultText += `O maior valor informado foi: ${largest}<br>`;
+  resultText += `A média dos valores é: ${medias}<br>`;
+
+  res.innerHTML = resultText;
+  console.log(resultText)
 }
-  
- 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
